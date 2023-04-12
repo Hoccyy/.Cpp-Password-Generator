2f4368ef36ffda52fdf902773c72d7f251bd8f3f
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-04-11 23:51:58 -0400

    Some line changes and added comments

--- main.cpp
+++ main.cpp
@@ -1,39 +1,36 @@
 #include <iostream>
 
 int main() {
-    std::string *pass = new std::string();
-    int *length = new int();
-    int *temp = new int();
+    std::string *pass = new std::string();//Password value declared
+    int *length = new int();//Size of password value
+    int *temp = new int();//Temporary character value storage
     
-    std::cout<< "Password length: ";
-    std::cin >> *length;
+    std::cout<< "Password length : ";//Asks user for password size
+    std::cin >> *length;//Gets the input and stores it 
     
-    std::srand(time(NULL));
+    std::srand(time(NULL));//Seed initialization
     
-    while (pass -> length() < *length){
-        *temp = char(std::rand() % 122);
-        if (*temp <= 90 && *temp >= 65){
+    while (pass -> length() < *length){//Keeps generating random values until they fit character ranges for the password
+        *temp = char(std::rand() % 122);//Storing character values temporarily
+        if (*temp <= 90 && *temp >= 65){//Range for capital-case letters
             *pass += *temp;
-            std::cout<<*temp << std::endl;
         }
-        if (*temp <= 122 && *temp >= 97){
+        if (*temp <= 122 && *temp >= 97){//Range for common-case letters
             *pass += *temp;
-            std::cout<<*temp << std::endl;
         }
-        if (*temp <= 57 && *temp >= 48){
+        if (*temp <= 57 && *temp >= 48){//Range for numbers
             *pass += *temp;
-            std::cout<<*temp << std::endl;
         }
-        if (*temp == 33 || *temp == 35 || *temp == 36){
+        if (*temp == 33 || *temp == 35 || *temp == 36){//Range for some symbols for randomness
             *pass += *temp;
-            std::cout<<*temp << std::endl;
         }
     }
+    //Memory freeing and displaying password
+    delete temp;
     
-    std::cout << *pass;
+    std::cout << *pass <<std::endl;
     
     delete length;
     delete pass;
-    delete temp;
     return 0;
 }
